15dda26af02cab7b318f239536c3688b6c012fa6
Giang Trong Duong <trongduong04112003@gmail.com>
2023-12-29 10:31:30 +0700

    Update installationDocker.sh

--- Script/installationDocker.sh
+++ Script/installationDocker.sh
@@ -1,6 +1,13 @@
+#  RMIT University Vietnam
+#  Course: COSC2767 Systems Deployment and Operations
+#  Semester: 2023C
+#  Assessment: Assignment 2
+#   Author: Giang Trong Duong
+#   ID: s3926135
+#   Created  28/12/2023
+#   Last modified: 29/12/2023
+#   Acknowledgement: 
 yum install docker -y
 useradd dockeradmin
 passwd dockeradmin
-rm -r /etc/ssh/sshd_config
-cp sshd_config /etc/ssh/
 service sshd reload
